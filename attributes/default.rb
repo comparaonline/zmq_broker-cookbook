d80628ea6bbee5ee2dd1754916ffec6b3e3bac7b
Gabriel Gayan <gabrielgayan@gmail.com>
2014-04-01 17:39:19 -0300

    don't setup broker flags by default

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,5 +3,5 @@ default.zmq_broker.group = 'zmq_broker'
 default.zmq_broker.home = "/var/lib/#{default.zmq_broker.user}"
 
 default.zmq_broker.env = 'production'
-default.zmq_broker.flags = '-ping_channel="ping" -ping_message="{\"action\": \"ping\"}" -ping_wait=60 -pub_address="tcp://*:5556" -pub_hwm=5 -sub_address="tcp://*:5555" -sub_hwm=5'
+default.zmq_broker.flags = ''
 default.zmq_broker.binary_url = 'https://github.com/comparaonline/zmq_broker_go/releases/download/latest/zmq_broker_go'
