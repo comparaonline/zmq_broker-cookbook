4b675e003650899c053d12bce135d51d87fc4974
Gabriel Gayan <gabrielgayan@gmail.com>
2014-04-02 09:04:35 -0300

    added version attribute

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 This file is used to list changes made in each version of zmq_broker.
 
+## 0.8.0:
+
+* added version attribute
+
 ## 0.7.3:
 
 * fixed flags usage
--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email 'ggayan@comparaonline.com'
 license          'All rights reserved'
 description      'Configures and deploys zmq_broker application to node'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.7.3'
+version          '0.8.0'
 
 depends 'apt', '~> 2.3.6'
 depends 'logrotate', '~> 1.5.0'
